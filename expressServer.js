af20d426830bf92c988493c510143b25c79535a9
Daniel Pencak <danielpencak@gmail.com>
2016-12-14 16:22:46 -0800

    Add capability to read full list of pets/add '404 Not Found' middleware/add app.listen()

--- expressServer.js
+++ expressServer.js
@@ -6,7 +6,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const petsPath = path.join(__dirname, 'pet.json');
+const petsPath = path.join(__dirname, 'pets.json');
 
 const express = require('express');
 const app = express();
@@ -14,3 +14,27 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
+
+app.get('/pets', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+
+      return res.sendStatus(500);
+    }
+
+    const pets = JSON.parse(petsJSON);
+
+    res.send(petsJSON);
+  });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
