a3e77de9aab532da581cea639237467adef3a3be
Daniel Pencak <danielpencak@gmail.com>
2016-12-15 15:24:50 -0800

    Add in ability to read the full list/add in port and listen module/add in middlware that will catch all 404 and 500 errors

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -21,3 +21,34 @@ app.use(morgan('dev'));
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+
+// Read all
+app.get('/pets', (_req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      return next(err);
+    }
+
+    const pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+app.use((_req, res, _next) => {
+  return res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+
+  return res.sendStatus(500);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
