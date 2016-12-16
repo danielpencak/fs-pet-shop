a3efce81eb0fbc7975305b8b3a27cb5368a1ba7e
Daniel Pencak <danielpencak@gmail.com>
2016-12-15 18:23:52 -0800

    Add ability to update and delete a pet in the list

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -84,6 +84,83 @@ app.post('/pets', (req, res, next) => {
   });
 });
 
+// Update
+app.patch('/pets/:index', (req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      return next(readErr);
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const index = Number.parseInt(req.params.index);
+
+    if (index < 0 || index >= pets.length || Number.isNaN(index)) {
+      return next();
+    }
+
+    const { kind, name } = req.body;
+    const age = Number.parseInt(req.body.age);
+
+    if (!kind && !name && !age) {
+      return res.sendStatus(400);
+    }
+
+    if (name) {
+      pets[index].name = name;
+    }
+
+    if (kind) {
+      pets[index].kind = kind;
+    }
+
+    if (age) {
+      if (Number.isNaN(age)) {
+        return res.sendStatus(400);
+      }
+
+      pets[index].age = age;
+    }
+
+    const newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
+      if (writeErr) {
+        return next(writeErr);
+      }
+
+      res.send(pets[index]);
+    });
+  });
+});
+
+// Delete
+app.delete('/pets/:index', (req, res, next) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      return next(readErr);
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const index = Number.parseInt(req.params.index);
+
+    if (index < 0 || index >= pets.length || Number.isNaN(index)) {
+      return next();
+    }
+
+    const pet = pets.splice(index, 1)[0];
+
+    const newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
+      if (writeErr) {
+        return next(writeErr);
+      }
+
+      res.send(pet);
+    });
+  });
+});
+
 app.use((_req, res, _next) => {
   return res.sendStatus(404);
 });
